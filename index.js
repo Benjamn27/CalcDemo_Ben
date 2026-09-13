02ab9e07028844e95712e8956ab4914923cd2337
Benjamn27 <ben.r.hartshorn@gmail.com>
2026-09-13 12:12:35 -0500

    All of my changes since I forgot to do them one at a time like a smart person

--- index.js
+++ index.js
@@ -5,24 +5,30 @@ const inputFieldEl = document.getElementById("result");
 for (let i = 0; i < allButtonEls.length; i++) {
   allButtonEls[i].addEventListener("click", () => {
     const buttonValue = allButtonEls[i].textContent;
-    if (buttonValue === "C") {
-      clearResult();
-    } else if (buttonValue === "=") {
-      calculateResult();
-    } else {
+    if (buttonValue === "Be Gone, Numbers!") {
+      eliminateResult();
+    } else if (buttonValue === "Gimme Answer!") {
+      bigBrainResult();
+    } else if (buttonValue === "27"){
+      myFavoriteNumber();
+    }else {
       appendValue(buttonValue);
     }
   });
 }
 
-function clearResult() {
+function eliminateResult() {
   inputFieldEl.value = "";
 }
 
-function calculateResult() {
-  inputFieldEl.value = eval(inputFieldEl.value);
+function bigBrainResult() {
+  inputFieldEl.value = eval(inputFieldEl.value)+"!!!!!!!";
 }
 
 function appendValue(buttonValue) {
   inputFieldEl.value += buttonValue;
+}
+
+function myFavoriteNumber(){
+  inputFieldEl.value = "27 in my favorite number, that is the only purpose for this button"
 }
